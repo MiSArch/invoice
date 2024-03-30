9bbb1569c69c59bf9b9235ea221bac8b8f357b40
st170001 <st170001@stud.uni-stuttgart.de>
2024-03-30 17:57:45 +0100

    Fixed InvoiceDTO to match Invoice

--- src/invoice.rs
+++ src/invoice.rs
@@ -94,12 +94,16 @@ fn build_order_item_invoice_content(value: &OrderEventData) -> String {
 #[derive(Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct InvoiceDTO {
+    pub order_id: Uuid,
+    pub issued_at: chrono::DateTime<chrono::Utc>,
     pub content: String,
 }
 
 impl From<Invoice> for InvoiceDTO {
     fn from(value: Invoice) -> Self {
         InvoiceDTO {
+            order_id: value.order_id,
+            issued_at: value.issued_at.to_chrono(),
             content: value.content,
         }
     }
